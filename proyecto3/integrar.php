1ed27f289e78ff0be057c62ad7c3e6320643c30c
Agustin Calderon <agustincl@gmail.com>
2015-01-22 11:14:16 +0100

    Added renderForm interface

--- proyecto3/integrar.php
+++ proyecto3/integrar.php
@@ -4,6 +4,7 @@
 include('userForm.php');
 include('filterForm.php');
 include('validateForm.php');
+include('renderForm.php');
 
 $filterdata = filterForm($userForm, $_POST);
 $validatedata = validateForm($userForm, $filterdata);
@@ -11,4 +12,5 @@ $validatedata = validateForm($userForm, $filterdata);
 if($validatedata)
 {
     // Guardar en un txt
+    
 }
---
+++ proyecto3/renderForm.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Render form
+ * 
+ * @param array $form definition
+ * @param array $data postdata
+ * @return string $html
+ */
+function renderForm($form, $data=null)
+{
+    return $html;
+}
