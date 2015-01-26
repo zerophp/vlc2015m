3b7fd747b045b34853b232387eda47ae4d0da3d4
Agustin Calderon <agustincl@gmail.com>
2015-01-26 13:45:14 +0100

    Seudocode for parseurl

--- crudtxt/modules/core/src/core/models/parseURL.php
+++ crudtxt/modules/core/src/core/models/parseURL.php
@@ -3,7 +3,34 @@
 
 function parseURL()
 {
-    $controllers = array ('usuarios'=>array('insert', 'select', 'update', 'delete'));
+    $controllers = array ('usuarios'=>array('insert', 'select', 'update', 'delete'),
+                          'error'=>array('404'),
+                          'index'=>array('index')
+    );
+    
+    // dividir la url en un array
+    
+    // Tiene parametros?
+    // Es longitud par?
+        // OK rellenar request
+        // KO deveolver error 412
+    
+    // No tiene parametros
+        // Tiene controller
+        // si
+            //Existe?
+        // no
+            // return index
+        
+        // Tiene action
+        // si
+            // Existe?
+        // no
+            //Return index
+    
+    // No tiene controller
+        // return index/index
+        
     
     $request = array();
     return $request;
